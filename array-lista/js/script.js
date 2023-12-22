2470170add6d48b11473ec59db183656403e1f7d
DaniAm94 <dani.amico94@gmail.com>
2023-12-22 16:19:00 +0100

    completed 1st exercise

---
+++ array-lista/js/script.js
@@ -0,0 +1,19 @@
+const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+console.log(fruits);
+
+const peach = 'pesca';
+
+fruits.push(peach);
+console.log(fruits);
+
+let found = false;
+let result = 'Oh no, devo uscire a comprare il cocomero!';
+
+for (let i = 0; i < fruits.length && !found; i++) {
+    const watermelon = 'cocomero';
+    if (fruits[i] === watermelon) {
+        found = true;
+        result = 'Trovato! Devo solo preparare il cocktail.'
+    }
+}
+console.log(result);
