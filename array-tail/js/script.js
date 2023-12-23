c4adc53148cc3d492f5e7808241c87f785eed8b8
DaniAm94 <dani.amico94@gmail.com>
2023-12-23 16:23:48 +0100

    added 2nd exercise's bonus

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -3,22 +3,25 @@ let isValid = true;
 
 // Chiedo all'utente quanti elementi vuole nell'array
 const nElements = parseInt(prompt('Quanti elementi ci devono essere nell\'array?'));
-
+const tailLength = parseInt(prompt('Quanto vuoi che sia lunga la coda da stampare?'))
 // Controlli di validazione l'utente deve inserire un numero e non deve essere inferiore a 6
-if (isNaN(nElements)) {
+if (isNaN(nElements) || isNaN(tailLength)) {
     isValid = false;
     alert('Sei Pregato di inserire un numero!')
-} else if (nElements < 6) {
+} else if (nElements < 1) {
     isValid = false;
-    alert('Devono esserci non meno di 6 elementi');
+    alert('Deve esserci almeno 1 elemento');
+} else if (tailLength > nElements) {
+    isValid = false;
+    alert('La coda di elementi da stampare non può essere più lunga dell\'array iniziale');
 }
-// Creo un array in cui inserire gli elementi
-const list = [];
-
-// Creo un array "coda" in cui inserire gli ultimi 5 elementi dell'array list
-const tail = [];
 
 if (isValid) {
+    // Creo un array in cui inserire gli elementi
+    const list = [];
+
+    // Creo un array "coda" in cui inserire gli ultimi 5 elementi dell'array list
+    const tail = [];
 
     // Implemento un ciclo for per riempire l'array list
     for (let i = 0; i < nElements; i++) {
@@ -28,14 +31,14 @@ if (isValid) {
         // Inserisco l'elemento nell'array list
         list[i] = randomElement;
 
-        // Se l'indice mi indica che siamo arrivati almeno al quint'ultimo elemento dell'array list  all'ora inserisco quell'elemento anchhe nell'array tail
-        if (i >= (nElements - 5)) {
+        // Se l'indice mi indica che siamo arrivati al primo elemento della coda all'ora inserisco quell'elemento anchhe nell'array tail
+        if (i >= (nElements - tailLength)) {
             tail.push(randomElement);
         }
     }
 
     // Stampo i due array
     console.table(list);
-    console.table(tail);
+    console.log('Coda: ', tail);
 }
 
