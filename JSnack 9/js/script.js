995c3ba64b13b04cfb42a2c0d1b3ad269f9efffb
DaniAm94 <dani.amico94@gmail.com>
2024-01-01 18:30:31 +0100

    completed 9th JSnack

---
+++ JSnack 9/js/script.js
@@ -0,0 +1,8 @@
+let sum = 0;
+let average = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+average = sum / 10;
+console.log(`La somma dei numeri da 1 a 10 è: ${sum}
+La media dei numeri da 1 a 10 è: ${average}`);
