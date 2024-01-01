1a6e79a3acc455e81c8f0a8a47053c4e18fe123d
DaniAm94 <dani.amico94@gmail.com>
2024-01-01 18:11:24 +0100

    completed 7th JSnack

---
+++ JSnack 7/README.md
@@ -0,0 +1,2 @@
+# JSnack 7
+## Stampa le potenze di 2 fino alla 1000sima.
---
+++ JSnack 7/js/script.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 1000; i++) {
+    const twoPow = Math.pow(2, i);
+    console.log(`2 alla ${i}a = ${twoPow}`);
+}
