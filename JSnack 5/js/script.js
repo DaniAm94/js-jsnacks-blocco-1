ed680444761dc3d04d92d34617a184b76f62cfb5
DaniAm94 <dani.amico94@gmail.com>
2024-01-01 17:47:50 +0100

    completed 5th JSnack

---
+++ JSnack 5/js/script.js
@@ -0,0 +1,14 @@
+const array = [];
+
+for (let i = 0; i < 6; i++) {
+    const num = parseInt(prompt('Inserisci un numero: '));
+    if (isNaN(num)) {
+        alert('Si prega di inserire un numero!');
+        i--;
+    } else {
+        console.log(num);
+        if (num % 2) array.push(num);
+    }
+}
+console.log('Lista dei numeri dispari che hai inserito:');
+console.table(array);
